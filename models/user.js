c8edee967bb245fc928e5f1fcb2d30fc93c19806
apolo-sys <cristian124421@gmail.com>
2021-05-14 05:01:03 +0200

    Added USER MODEL

---
+++ models/user.js
@@ -0,0 +1,42 @@
+const { Schema, model } = require('mongoose');
+
+
+const UserSchema = Schema({
+    name: {
+        type: String,
+        required: [true, 'Missing Name']
+    },
+    mail: {
+        type: String,
+        required: [true, 'Missing Mail'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Missing Password']
+    },
+    img: {
+        type: String
+    },
+    role: {
+        type: String,
+        required: true,
+        enum: ['ADMIN_ROLE', 'USER_ROLE']
+    },
+    state: {
+        type: Boolean,
+        default: true
+    },
+    google: {
+        type: Boolean,
+        default: false
+    }
+})
+
+
+
+
+
+module.exports = {
+
+}
